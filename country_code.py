70a62970c15e6ca2fc61916c141930a0fd11991d
이재혁 <nespot2@gmail.com>
2019-06-16 19:36:21 +0900

    project 시작

---
+++ country_code.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_html(url):
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        return resp.text
+    return ""
+
+
+def list_to_str(gen):
+    return "|".join(gen)
+
+
+def soup_to_dict(soup):
+    td_arr = soup.find_all("td")
+    generator = map(lambda v: v.get_text().strip(), td_arr)
+    return list_to_str(generator)
+
+
+html = get_html("https://ko.wikipedia.org/wiki/ISO_3166-1")
+
+soup = BeautifulSoup(html, 'html.parser')
+
+tr = soup.find("table", {"class": "wikitable sortable"}).find("tbody").findAll("tr")
+
+head, *tail = tr
+
+result = map(soup_to_dict, tail)
+
+# with open("country_code", "w") as f:
+#     f.write("\n".join(result))
